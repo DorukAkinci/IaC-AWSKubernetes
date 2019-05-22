ddb4b03c955c28dcf0b3ef499aa3f69eed06d99b
root <root@ip-172-31-17-109.us-west-2.compute.internal>
2019-05-22 06:11:04 +0000

    Added DependsOn properties for fixing the problem about cannot be destroyed

--- Terraform/iam.tf
+++ Terraform/iam.tf
@@ -21,11 +21,13 @@ POLICY
 resource "aws_iam_role_policy_attachment" "tf-cluster-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = "${aws_iam_role.tf-eks-master.name}"
+  depends_on = ["aws_iam_role.tf-eks-master"]
 }
  
 resource "aws_iam_role_policy_attachment" "tf-cluster-AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
   role = "${aws_iam_role.tf-eks-master.name}"
+  depends_on = ["aws_iam_role.tf-eks-master"]
 }
 
 resource "aws_iam_role" "tf-eks-node" {
@@ -50,20 +52,24 @@ POLICY
 resource "aws_iam_role_policy_attachment" "tf-eks-node-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = "${aws_iam_role.tf-eks-node.name}"
+  depends_on = ["aws_iam_role.tf-eks-node"]
 }
  
 resource "aws_iam_role_policy_attachment" "tf-eks-node-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = "${aws_iam_role.tf-eks-node.name}"
+  depends_on = ["aws_iam_role.tf-eks-node"]
 }
  
 resource "aws_iam_role_policy_attachment" "tf-eks-node-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = "${aws_iam_role.tf-eks-node.name}"
+  depends_on = ["aws_iam_role.tf-eks-node"]
 }
 
 
 resource "aws_iam_instance_profile" "node" {
   name = "terraform-eks-node"
   role = "${aws_iam_role.tf-eks-node.name}"
+  depends_on = ["aws_iam_role.tf-eks-node"]
 }
